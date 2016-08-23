f24d0e0b6b7b896c247b6838c309b295ae0f0e97
Supriya2807 <lahadesupriya@gmail.com>
2016-08-23 14:53:52 +0530

    Session13Assign1
    Session13Assign1

---
+++ Session13Assign1/app/src/main/java/com/supriyalahade/session13assign1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.supriyalahade.session13assign1;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    ActionBar actionBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager=(ViewPager)findViewById(R.id.viewPager);
+
+        actionBar=getSupportActionBar();
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_layout,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
